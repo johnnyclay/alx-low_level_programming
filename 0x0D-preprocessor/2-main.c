6e349278c12fe5d64a59be8f1ccc802cfe299fcf
johnnyclay <kidusyohannes456@gmail.com>
2022-04-12 16:51:13 +0000

    fixing 0 and 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from.
+ * main - prints the name of the file.
  *
- * Return: Always 0 Success.
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n" , __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
