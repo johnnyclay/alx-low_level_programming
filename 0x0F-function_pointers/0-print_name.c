78ecee552fef66e108cec5c39fa2d48594435b6a
root <kidusyohannes456@gmail.com>
2022-04-13 10:29:48 -0700

    fixing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 
 /**
- * print_name - print name as is.
- *@name - name of the person
+ *print_name - print name as is.
+ *@name: name of the person
  *
  *@f: pointer to the printing function
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char*))
 {
