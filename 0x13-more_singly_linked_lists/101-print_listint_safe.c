9e172bae59fd342ed613bd8e420c90593f358c13
root <kidusyohannes456@gmail.com>
2022-04-25 11:00:18 -0700

    fixing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint_safe - prints all the elements of a linked list
@@ -31,23 +32,3 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	return (count);
 }
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
-
